71d51a07d9b8a15a2d73d0ade002a19c661fbbec
Wahid Nawzadi <dev@wahidnawzadi.com>
2023-10-08 09:40:45 +0200

    change build

--- 03.astro-view-animation/astro.config.mjs
+++ 03.astro-view-animation/astro.config.mjs
@@ -1,4 +1,3 @@
-import nodejs from "@astrojs/node";
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
@@ -6,8 +5,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: nodejs({
-    mode: "middleware", // or 'standalone'
-  }),
-  output: "hybrid",
 });
