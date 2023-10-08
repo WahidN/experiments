1795d5b35bf2bf5bc5f56214d9f7a276d0ab69c0
Wahid Nawzadi <dev@wahidnawzadi.com>
2023-10-08 09:28:51 +0200

    Add rick and morty api

--- 03.astro-view-animation/astro.config.mjs
+++ 03.astro-view-animation/astro.config.mjs
@@ -1,8 +1,13 @@
+import nodejs from "@astrojs/node";
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: nodejs({
+    mode: "middleware", // or 'standalone'
+  }),
+  output: "hybrid",
 });
---
+++ 03.astro-view-animation/types.ts
@@ -0,0 +1,12 @@
+export type Character = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
